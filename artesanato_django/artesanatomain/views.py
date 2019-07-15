2ad99cadf5ab6c6afe5544fcc267975b230add96
Marcos Paulo Diniz <marcosdinizpaulo@gmail.com>
2019-07-14 23:19:22 -0300

    ajustanto o arquivo de views para adicionar as novas páginas e passar os dados para o front end por parametro para ficarem dinamicos

--- artesanato_django/artesanatomain/views.py
+++ artesanato_django/artesanatomain/views.py
@@ -11,19 +11,29 @@ def indexView(request):
     tecnicas = Tecnica.objects.all()
     context = {'posts':
                 [
-                    {'titulo': 'Post #1', 'conteudo': 'asdfghjklç', 'img': 'https://http2.mlstatic.com/artesanato-do-piaui-ilha-santa-isabel-em-palha-de-carnauba-D_NQ_NP_724041-MLB27111520895_042018-F.jpg'},
-                    {'titulo': 'Post #2', 'conteudo': 'zxcvbnm', 'img': ''}
+                    {'titulo': 'Post #1', 'conteudo': 'asdfghjklç', 'img': 'https://http2.mlstatic.com/artesanato-do-piaui-ilha-santa-isabel-em-palha-de-carnauba-D_NQ_NP_724041-MLB27111520895_042018-F.jpg', 'id': '1'},
+                    {'titulo': 'Post #2', 'conteudo': 'zxcvbnm', 'img': '', 'id' : '2'}
                 ],
                 'tecnicas':tecnicas
             }
     return render(request, 'artesanatoMain/index.html', context)
 
 def historiaView(request, historia_id):
+    tecnicas = Tecnica.objects.all()
     historia = get_object_or_404(Historia, pk=historia_id)
-    context={'historia':historia}
+    # artesao = get_object_or_404(Artesao, pk=historia.artesao_id)
+    pessoa = get_object_or_404(Pessoa, pk=historia.artesao.pessoa_id)
+    context={'historia':historia, 'nome':pessoa.nome, 'tecnicas': tecnicas}
     return render(request, 'artesanatoMain/historia.html', context)
 
 def tecnicaView(request, tecnica_id):
+    tecnicas = Tecnica.objects.all()
+
     tecnica = get_object_or_404(Tecnica, pk=tecnica_id)
-    context={'tecnica':tecnica}
+    context={'tecnica':tecnica, 'tecnicas': tecnicas}
+    print(tecnica.nome, tecnica.descricao)
     return render(request, 'artesanatoMain/tecnica.html', context)
+
+def descubraView(request):
+    context={}
+    return render(request, 'artesanatoMain/descubra.html', context)
