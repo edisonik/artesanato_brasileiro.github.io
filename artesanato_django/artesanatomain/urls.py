02a084604a04e73b7b17d3fbe4146805294ec245
Marcos Paulo Diniz <marcosdinizpaulo@gmail.com>
2019-07-14 23:17:17 -0300

    adicionando url da página descubra

--- artesanato_django/artesanatomain/urls.py
+++ artesanato_django/artesanatomain/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	url(r'^$',views.indexView, name='index'), #index
     url(r'^Historia/(?P<historia_id>[0-9]+)$', views.historiaView),
     url(r'^Tecnica/(?P<tecnica_id>[0-9]+)$', views.tecnicaView),
+    url(r'^Descubra/', views.descubraView),
 ]
